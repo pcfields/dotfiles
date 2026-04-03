0508cd909541327e2146bbc66f126f024464bbec
Peter Fields <pcfields@gmail.com>
2026-04-03 02:57:38 +0200

    fix copilot.lua: call is_windows_platform() instead of assigning function ref

--- lua/pcf/plugins/ai/copilot.lua
+++ lua/pcf/plugins/ai/copilot.lua
@@ -1,7 +1,7 @@
 return {
 	"github/copilot.vim",
 	config = function()
-		local on_outdated_work_computer = require("pcf.utils").is_windows_platform
+		local on_outdated_work_computer = require("pcf.utils").is_windows_platform()
 
 		if on_outdated_work_computer then
 			vim.g.copilot_node_command = "C:/Users/PeterFields/AppData/Local/nvm/v24.4.0/node.exe"
