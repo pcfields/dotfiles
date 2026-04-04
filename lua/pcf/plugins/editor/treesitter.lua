8be2364c34b93569e262e23fd6928db5bfee7b85
Peter Fields <pcfields@gmail.com>
2026-04-04 18:17:20 +0200

    migrate nvim-treesitter to new 0.12 API: setup/install, FileType highlighting, standalone textobjects

--- lua/pcf/plugins/editor/treesitter.lua
+++ lua/pcf/plugins/editor/treesitter.lua
@@ -1,56 +1,82 @@
 -- https://github.com/nvim-treesitter/nvim-treesitter
--- [[ Configure Treesitter ]]
--- See `:help nvim-treesitter`
+-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 
-return { -- Highlight, edit, and navigate code
+return { -- Treesitter: parser management, highlighting, textobjects
 	"nvim-treesitter/nvim-treesitter",
-	event = "BufRead",
+	lazy = false,
+	build = ":TSUpdate",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
-		"mfussenegger/nvim-ts-hint-textobject",
 		"windwp/nvim-ts-autotag",
 	},
-	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- Add languages to be installed here that you want installed for treesitter
-			ensure_installed = {
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"tsx",
-				"typescript",
-				"elm",
-				"haskell",
-				"rust",
-			},
+		local map = require("pcf.utils").map
 
-			-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-			auto_install = false,
-			highlight = {
-				enable = true,
-			},
-		indent = {
-			enable = true,
-			disable = { "python" },
-		},
-		-- NOTE: incremental_selection replaced by native v_an/v_in in Neovim 0.12+
-		-- Use `an` to expand selection outward and `in` to shrink inward in visual mode
-		-- Enable syntax aware text objects
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
+		-- Parser management
+		require("nvim-treesitter").setup({})
+
+		-- Install parsers (no-op if already installed)
+		require("nvim-treesitter").install({
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"tsx",
+			"typescript",
+			"elm",
+			"haskell",
+			"rust",
+		})
+
+		-- Enable treesitter highlighting per filetype (replaces highlight.enable)
+		local ts_filetypes = {
+			"html",
+			"javascript",
+			"javascriptreact",
+			"json",
+			"lua",
+			"typescript",
+			"typescriptreact",
+			"elm",
+			"haskell",
+			"rust",
+			"css",
+			"markdown",
+			"yaml",
+			"toml",
+		}
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = ts_filetypes,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+
+		-- Textobjects (standalone API)
+		require("nvim-treesitter-textobjects").setup({
+			select = {
+				lookahead = true,
 			},
 		})
+
+		local select_textobject = require("nvim-treesitter-textobjects.select").select_textobject
+
+		map({ "x", "o" }, "af", function()
+			select_textobject("@function.outer", "textobjects")
+		end, { desc = "Select outer function" })
+
+		map({ "x", "o" }, "if", function()
+			select_textobject("@function.inner", "textobjects")
+		end, { desc = "Select inner function" })
+
+		map({ "x", "o" }, "ac", function()
+			select_textobject("@class.outer", "textobjects")
+		end, { desc = "Select outer class" })
+
+		map({ "x", "o" }, "ic", function()
+			select_textobject("@class.inner", "textobjects")
+		end, { desc = "Select inner class" })
 
 		-- Setup rename open & closing html tag
 		require("nvim-ts-autotag").setup({})
