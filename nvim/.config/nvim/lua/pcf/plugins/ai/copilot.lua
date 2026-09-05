ca1d14d882b98293c39cc0612b3a6b2e11139780
Peter Fields <pcfields@gmail.com>
2026-09-05 22:20:20 +0200

    fix(nvim): use PATH for Copilot Node

--- nvim/.config/nvim/lua/pcf/plugins/ai/copilot.lua
+++ nvim/.config/nvim/lua/pcf/plugins/ai/copilot.lua
@@ -1,10 +1,3 @@
 return {
 	"github/copilot.vim",
-	config = function()
-		local on_outdated_work_computer = require("pcf.utils").is_windows_platform()
-
-		if on_outdated_work_computer then
-			vim.g.copilot_node_command = "C:/Users/PeterFields/AppData/Local/nvm/v24.4.0/node.exe"
-		end
-	end,
 }
